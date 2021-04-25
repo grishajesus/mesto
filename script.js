f69f19a729a4620bfcad70e01b78b71a8fc68e06
Grigory Sivkov <sivkov12345@mail.ru>
2021-04-25 23:45:28 +0300

    fix2

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 let popup = document.querySelector('.popup');
-let formElement = popup.querySelector('.popup_content');
+let formElement = popup.querySelector('.popup__content');
 let closePopupPopup = document.querySelector('.popup-edit__close');
-let openPopupButton = document.querySelector('.user-info__edit');
-let nameInput = document.querySelector('#name');
-let jobInput = document.querySelector('#about');
+let openPopupButton = document.querySelector('user-info__button');
+let nameInput = document.querySelector('name');
+let jobInput = document.querySelector('about');
 let title = document.querySelector('user-info__name');
 let subtitle = document.querySelector('.user-info__job');
 
@@ -13,11 +13,11 @@ function formSubmitHandler (event) {
     let name = nameInput.value;
     title.textContent = name;
     subtitle.textContent = job;
-    popup.classList.remove('popup_is-opened');
+    popup.classList.remove('popup-edit_is-opened');
 }
 
 function formCloseHandler () {
-    popup.classList.remove('popup_is-opened');
+    popup.classList.remove('popup-edit_is-opened');
 }
 
 function formOpenHandler () {
@@ -25,7 +25,7 @@ function formOpenHandler () {
     let job = subtitle.textContent;
     nameInput.value = name;
     jobInput.value = job;
-    popup.classList.add('popup_is-opened');
+    popup.classList.add('popup-edit_is-opened');
 }
 formElement.addEventListener('submit', formSubmitHandler);
 closePopupPopup.addEventListener('click', formCloseHandler);
