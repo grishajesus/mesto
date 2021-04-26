c9483654aeae7a2879916f9a4efbe25cdcbb6eee
Grigory Sivkov <sivkov12345@mail.ru>
2021-04-26 10:34:37 +0300

    mesto

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 let popup = document.querySelector('.popup');
 let formElement = popup.querySelector('.popup__content');
-let closePopupPopup = document.querySelector('.popup-edit__close');
-let openPopupButton = document.querySelector('user-info__button');
-let nameInput = document.querySelector('name');
-let jobInput = document.querySelector('about');
-let title = document.querySelector('user-info__name');
-let subtitle = document.querySelector('.user-info__job');
+let closePopupPopup = document.querySelector('.popup__close-button')
+let openPopupButton = document.querySelector('.profile__edit-button');
+let nameInput = document.querySelector('#name');
+let jobInput = document.querySelector('#about');
+let title = document.querySelector('.profile__name');
+let subtitle = document.querySelector('.profile__job');
 
 function formSubmitHandler (event) {
     event.preventDefault();
@@ -13,11 +13,12 @@ function formSubmitHandler (event) {
     let name = nameInput.value;
     title.textContent = name;
     subtitle.textContent = job;
-    popup.classList.remove('popup-edit_is-opened');
+    popup.classList.remove('popup_opened');
+
 }
 
 function formCloseHandler () {
-    popup.classList.remove('popup-edit_is-opened');
+    popup.classList.remove('popup_opened');
 }
 
 function formOpenHandler () {
@@ -25,8 +26,10 @@ function formOpenHandler () {
     let job = subtitle.textContent;
     nameInput.value = name;
     jobInput.value = job;
-    popup.classList.add('popup-edit_is-opened');
+    popup.classList.add('popup_opened');
 }
+    
+
 formElement.addEventListener('submit', formSubmitHandler);
 closePopupPopup.addEventListener('click', formCloseHandler);
 openPopupButton.addEventListener ('click', formOpenHandler);
