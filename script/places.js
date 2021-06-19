b3d5e05ef1710b7431987a2970581fc0d7685810
Grigory Sivkov <sivkov12345@mail.ru>
2021-06-19 14:13:40 +0300

    autotest

--- script/places.js
+++ script/places.js
@@ -3,13 +3,11 @@ import { cardImagePopup, openPopup } from "./common.js";
 const initialCards = [
 	{
 		title: "Архыз",
-		link:
-			"https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+		link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
 	},
 	{
 		title: "Челябинская область",
-		link:
-			"https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+		link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
 	},
 	{
 		title: "Иваново",
@@ -18,18 +16,15 @@ const initialCards = [
 	},
 	{
 		title: "Камчатка",
-		link:
-			"https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+		link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
 	},
 	{
 		title: "Холмогорский район",
-		link:
-			"https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+		link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
 	},
 	{
 		title: "Байкал",
-		link:
-			"https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+		link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
 	},
 ];
 
